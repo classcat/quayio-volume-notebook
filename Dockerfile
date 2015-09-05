014c6aa0952326c712ab43b3e7f350b894c93121
ClassCat <support@classcat.com>
2015-09-05 15:11:53 +0900

    init

---
+++ Dockerfile
@@ -0,0 +1,48 @@
+FROM ubuntu:trusty
+MAINTAINER ClassCat Co.,Ltd. <support@classcat.com>
+
+########################################################################
+# ClassCat/Spark Dockerfile
+#   Maintained by ClassCat Co.,Ltd ( http://www.classcat.com/ )
+########################################################################
+
+#--- HISTORY -----------------------------------------------------------
+# 04-sep-15 : fixed.
+#
+#--- TODO --------------------------------------------------------------
+# 04-sep-15 : should be run as non-root.
+#
+#--- DESCRIPTION -------------------------------------------------------
+# 04-sep-15 : python-numpy, python-matplotlib, python-nose : o.k.
+#
+#-----------------------------------------------------------------------
+
+ENV DEBIAN_FRONTEND noninteractive
+
+RUN apt-get update && apt-get -y upgrade \
+  && apt-get install -y language-pack-en language-pack-en-base \
+  && apt-get install -y language-pack-ja language-pack-ja-base \
+  && update-locale LANG="en_US.UTF-8" \
+  && apt-get install -y openssh-server supervisor rsyslog \
+  && mkdir -p /var/run/sshd \
+  && sed -i.bak -e "s/^PermitRootLogin\s*.*$/PermitRootLogin yes/" /etc/ssh/sshd_config
+
+COPY assets/supervisord.conf /etc/supervisor/supervisord.conf
+
+# OpenJDK 8
+#RUN apt-get install -y software-properties-common \
+#  && apt-add-repository ppa:openjdk-r/ppa \
+#  && apt-get update \
+#  && apt-get install -y openjdk-8-jdk
+
+VOLUME /notebook
+
+WORKDIR /opt
+COPY assets/cc-init.sh /opt/cc-init.sh
+
+#EXPOSE 22 80
+
+CMD /opt/cc-init.sh; /usr/bin/supervisord -c /etc/supervisor/supervisord.conf
+
+
+### End of Dockerfile ###
---
+++ assets/cc-init.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+########################################################################
+# ClassCat/Spark Asset files
+# Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved.
+########################################################################
+
+#--- HISTORY -----------------------------------------------------------
+#-----------------------------------------------------------------------
+
+
+######################
+### INITIALIZATION ###
+######################
+
+function init () {
+  echo "ClassCat Info >> initialization code for ClassCat/Spark"
+  echo "Copyright (C) 2015 ClassCat Co.,Ltd. All rights reserved."
+  echo ""
+}
+
+############
+### SSHD ###
+############
+
+function change_root_password() {
+  if [ -z "${ROOT_PASSWORD}" ]; then
+    echo "ClassCat Warning >> No ROOT_PASSWORD specified."
+  else
+    echo -e "root:${ROOT_PASSWORD}" | chpasswd
+    # echo -e "${password}\n${password}" | passwd root
+  fi
+}
+
+
+function put_public_key() {
+  if [ -z "$SSH_PUBLIC_KEY" ]; then
+    echo "ClassCat Warning >> No SSH_PUBLIC_KEY specified."
+  else
+    mkdir -p /root/.ssh
+    chmod 0700 /root/.ssh
+    echo "${SSH_PUBLIC_KEY}" > /root/.ssh/authorized_keys
+  fi
+}
+
+
+##################
+### SUPERVISOR ###
+##################
+# See http://docs.docker.com/articles/using_supervisord/
+
+function proc_supervisor () {
+  cat > /etc/supervisor/conf.d/supervisord.conf <<EOF
+[program:ssh]
+command=/usr/sbin/sshd -D
+
+[program:rsyslog]
+command=/usr/sbin/rsyslogd -n
+EOF
+}
+
+
+
+### ENTRY POINT ###
+
+init 
+change_root_password
+put_public_key
+proc_supervisor
+
+exit 0
+
+
+### End of Script ###
+
